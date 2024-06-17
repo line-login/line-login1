e3debbbcbecdf8c389a05c6d4901e24816f17da3
yeong05 <sasachen0719@gmail.com>
2024-06-17 11:37:14 +0800

    0617

--- laundry/a_index.php
+++ laundry/a_index.php
@@ -5,7 +5,7 @@
     <?php include("db.php"); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>洗衣店</title>
+    <title>洗衣店登入</title>
     <style>
         .container {
             flex: 1;
